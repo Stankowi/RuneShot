5aa46acbc0d1498fa0de6cdb3309df73789314ee
Eric Merritt <emmerrit@amazon.com>
2013-12-24 11:46:52 -0800

    support player respawn

--- Assets/Game/Scripts/Character/Health.js
+++ Assets/Game/Scripts/Character/Health.js
@@ -1,12 +1,26 @@
-private var health: int = 100;
+private static var defaultHealth: int = 100;
+private var health: int = defaultHealth;;
 
 function ResolveDamage(damage: int) {
-   health -= damage;
-   Debug.Log("Health is now " + health);
-   
-   if (health <= 0) {
+    health -= damage;
+    Debug.Log("Health is now " + health);
+
+    if (health <= 0) {
         // probably not the right way to handle death, but works for now.
-        Destroy(gameObject);
-   }
+        Die();
+    }
 }
 
+function Die() {
+    var spawnPoints: GameObject[] = GameObject.FindGameObjectsWithTag("SpawnPoint");
+    var pointIndex: int = Random.Range(0.0, spawnPoints.length);
+    var spawnPoint: GameObject = spawnPoints[pointIndex];
+
+    ResetHealth();
+    gameObject.transform.position = spawnPoint.transform.position;
+}
+
+
+function ResetHealth() {
+    health = defaultHealth;
+}
