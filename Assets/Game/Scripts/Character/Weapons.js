875013a837ec20e95ad5db8cab973bbd0002be48
Eric Merritt <emmerrit@amazon.com>
2013-12-27 13:00:16 -0800

    minor refactoring and cleanup

--- Assets/Game/Scripts/Character/Weapons.js
+++ Assets/Game/Scripts/Character/Weapons.js
@@ -2,6 +2,7 @@ private var powerCalcStart: int = 0;
 private var bouncyGranade: GameObject = null;
 private var currentWeapon: WeaponDesc = null;
 private var weaponList: Hashtable = new Hashtable();
+private var serverCache: Hashtable = new Hashtable();
 
 enum WeaponType {
     WeaponProducesProjectile,
@@ -27,11 +28,9 @@ class WeaponDesc {
 function Start() {
     bouncyGranade = Resources.Load("Guns/BouncyGranade", GameObject);
 
-
     // Setup the weapons list
     weaponList["bouncyGranade"] = new WeaponDesc(bouncyGranade, "BouncyGranade", WeaponType.WeaponIsProjectile);
     currentWeapon = weaponList["bouncyGranade"];
-
 }
 
 function Update () {
@@ -45,54 +44,87 @@ function Update () {
 }
 
 function StartPowerCalc() {
-    powerCalcStart = Time.time;
+    if (CallRemote()) {
+        networkView.RPC("StartPowerCalc",
+                        RPCMode.Server,
+                        Network.player);
+    } else {
+        powerCalcStart = Time.time;
+    }
+}
+
+@RPC
+function StartPowerCalc(networkPlayer: NetworkPlayer) {
+    serverCache[NetworkPlayer] = Time.time;
 }
 
 function EndPowerCalc() {
-    if (this.powerCalcStart == 0) {
+    var facing: Vector3 = FacingVector();
+
+    if (CallRemote()) {
+        networkView.RPC("EndPowerCalc",
+                        RPCMode.Server,
+                        Network.player,
+                        transform.position,
+                        facing);
+        return null;
+    }
+
+   if (this.powerCalcStart == 0) {
         return null;
     }
 
     var end = Time.time;
-
-    Trigger(FacingVector(), Power(powerCalcStart, end));
-
+    Trigger(transform.position, facing, end - this.powerCalcStart);
     powerCalcStart = 0;
-
     return null;
 }
 
-function Trigger(facing: Vector3, force: int) {
+@RPC
+function EndPowerCalc(networkPlayer: NetworkPlayer, position: Vector3, facing: Vector3) {
+    var powerCalcStart: int = serverCache[networkPlayer];
+    if (! powerCalcStart) {
+        return null;
+    }
+
+    var end = Time.time;
+    Trigger(position, facing, end - powerCalcStart);
+    serverCache[networkPlayer] = 0;
+    return null;
+}
+
+function Trigger(position: Vector3, facing: Vector3, pressDuration: int) {
+
     if (currentWeapon == null) {
         return null;
     }
 
     switch (currentWeapon.type) {
         case WeaponType.WeaponProducesProjectile:
-        TriggerProjectileWeapon(currentWeapon, facing, force);
+        TriggerProjectileWeapon(currentWeapon, position, facing, pressDuration);
         break;
-        case WeaponType.WeaponIsProjectile:
-        TriggerNonProjectileWeapon(currentWeapon, facing, force);
+    case WeaponType.WeaponIsProjectile:
+        TriggerNonProjectileWeapon(currentWeapon, position, facing, pressDuration);
         break;
     }
 
      return null;
 }
 
-function TriggerProjectileWeapon(weaponDesc: WeaponDesc, facing: Vector3, force: int) {
+function TriggerProjectileWeapon(weaponDesc: WeaponDesc, position: Vector3, facing: Vector3, pressDuration: int) {
     return null;
 }
 
-function TriggerNonProjectileWeapon(weaponDesc: WeaponDesc, facing: Vector3, force: int) {
-    var weaponPos = transform.TransformPoint(0, 1, 1);
+function TriggerNonProjectileWeapon(weaponDesc: WeaponDesc, position: Vector3, facing: Vector3, pressDuration: int) {
+    var weaponPos = position + Vector3(0,1,0);
     var weapon = Instantiate(weaponDesc.obj, weaponPos, Quaternion.identity);
-    weaponDesc.Component(weapon).Trigger(gameObject, facing, force);
-}
-
-function Power(start : int, end : int): int {
-    return Mathf.Min((end - start) * 1000, 10000);
+    weaponDesc.Component(weapon).Trigger(gameObject, facing, pressDuration);
 }
 
 function FacingVector() {
     return Camera.main.transform.forward;
+}
+
+function CallRemote(): boolean {
+    return gameObject.GetComponent("Character").CallRemote();
 }
--- Assets/Game/Scripts/Weapons/BouncyGranade.js
+++ Assets/Game/Scripts/Weapons/BouncyGranade.js
@@ -9,52 +9,51 @@ function Update() {
     }
 }
 
-function OnCollisionEnter(collision : Collision) {
+function OnCollisionEnter(collision: Collision) {
     var other: GameObject = collision.gameObject;
-    Debug.Log("Collisions " + (other.GetInstanceID() == this.launchingPlayer.GetInstanceID()));
     if (other.tag == "Player" &&
         (collisions > 0 ||
          (collisions <= 0 && ! IsLauncher(other)))) {
-        Debug.Log("Collided xxx");
         Explode();
     } else if (other != lastCollider) {
         collisions += 1;
         lastCollider = other;
-        Debug.Log("Collided " + collisions + " times");
     }
 }
 
-function IsLauncher(obj: GameObject) {
+function IsLauncher(obj: GameObject): boolean {
     return obj.GetInstanceID() == this.launchingPlayer.GetInstanceID();
 }
 
-function GetDamage() {
+function GetDamage(): int {
     return Mathf.Pow(power, (collisions + 1));
  }
 
- function GetDamageRadius() {
+ function GetDamageRadius(): int {
     return power * collisions;
  }
 
  function Explode() {
     var damageRadius: int = GetDamageRadius();
-    Debug.Log("Damage radius is " + damageRadius);
 
-    var colliders : Collider[] = Physics.OverlapSphere(transform.position, damageRadius);
+    var colliders: Collider[] = Physics.OverlapSphere(transform.position, damageRadius);
 
      var damage: int = GetDamage();
      for(var hit in colliders) {
         if(hit.gameObject.tag == "Player") {
-            //hit.rigidbody.AddExplosionForce(power, transform.position, damageRadius, 3.0);
             hit.gameObject.GetComponent("Health").ResolveDamage(damage);
-
         }
      }
      Destroy(gameObject);
-
  }
 
-function Trigger(launchingPlayer: GameObject, facing: Vector3, force: int) {
+@RPC
+function Trigger(launchingPlayer: GameObject, facing: Vector3, pressDuration: int) {
     this.launchingPlayer = launchingPlayer;
-    rigidbody.AddRelativeForce(facing * force, ForceMode.Force);
+    rigidbody.AddRelativeForce(facing * Power(pressDuration));
+}
+
+function Power(pressDuration: int): int {
+    var power: int = Mathf.Min(Mathf.Pow(2, pressDuration + 1), 64);
+    return Mathf.Min(power * 250, 5000);
 }
