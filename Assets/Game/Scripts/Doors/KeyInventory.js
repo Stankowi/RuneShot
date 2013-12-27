2c12102e4486ded9e63701f1a019b424f0135d9c
Adam Villalobos <avvillal@amazon.com>
2013-12-26 16:32:07 -0800

    Adding in keyInventory UI

--- Assets/Game/Scripts/Doors/KeyInventory.js
+++ Assets/Game/Scripts/Doors/KeyInventory.js
@@ -1,5 +1,8 @@
 
 private var keys: boolean[];
+var redKeyTex: Texture2D;
+var blueKeyTex: Texture2D;
+var yellowKeyTex: Texture2D;
 
 function hasKey(color : DoorColor) : boolean {
     return keys[color];
@@ -25,3 +28,28 @@ function Start () {
 function Update () {
 }
 
+function OnGUI () {
+
+    var keyWidth = 64;
+    var keyHeight = 64;
+    var margin = 10;
+    
+    var titleWidth = (keyWidth*3+margin*3);
+    var titleHeight = Screen.height/20;
+    var y = titleHeight*2 + margin;
+    
+    if( keys[DoorColor.Red] ) {
+    
+        GUI.DrawTexture(Rect(Screen.width-(keyWidth*3+margin*3),y,keyWidth,keyHeight), redKeyTex);
+    }
+    
+    if( keys[DoorColor.Blue] ) {
+        GUI.DrawTexture(Rect(Screen.width-(keyWidth*2+margin*2),y,keyWidth,keyHeight), blueKeyTex);
+    }
+    
+    if( keys[DoorColor.Yellow] ) {
+        GUI.DrawTexture(Rect(Screen.width-(keyWidth*1+margin*1),y,keyWidth,keyHeight), yellowKeyTex);
+    }
+    
+    GUI.Box(Rect(Screen.width-titleWidth,titleHeight,Screen.width/5,titleHeight),"KeyInventory");
+}
