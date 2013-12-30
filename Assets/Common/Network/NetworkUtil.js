6625f197f46d12c8188d8de77cf5e22711d39a77
Stankowi <stankowi+github@amazon.com>
2013-12-30 13:38:55 -0800

    Added NetworkUtil, which contains an Instantiate function that will automatically fall back to GameObject.Instantiate if there are no active connections.

---
+++ Assets/Common/Network/NetworkUtil.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+public static function Instantiate(prefab: UnityEngine.Object, position: Vector3, rotation: Quaternion, group: int) {
+
+     //Network.Instantiate(Resources.Load("Particles/Explosion"),transform.position,Quaternion.identity,20);
+     if(Network.connections.Length > 0) {
+        return Network.Instantiate(prefab,position,rotation,group);
+     }
+     else {
+        return GameObject.Instantiate(prefab,position,rotation);
+     }
+}
+
