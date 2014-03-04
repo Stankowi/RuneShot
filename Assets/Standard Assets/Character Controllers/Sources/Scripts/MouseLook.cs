e09a81a1ea4137b725269d762cced29bc9306623
MartinZ <zolniery@amazon.com>
2014-03-03 17:19:35 -0800

    Testing first commit -Added keyboard look -Fixed texture warping for some ramps

--- Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
@@ -19,8 +19,11 @@ public class MouseLook : MonoBehaviour {
 
 	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
 	public RotationAxes axes = RotationAxes.MouseXAndY;
-	public float sensitivityX = 15F;
-	public float sensitivityY = 15F;
+	public float sensitivityX = 4F;
+	public float sensitivityY = 4F;
+
+    public float sensitivityXKey = 4F;
+    public float sensitivityYKey = 2F;
 
 	public float minimumX = -360F;
 	public float maximumX = 360F;
@@ -28,7 +31,7 @@ public class MouseLook : MonoBehaviour {
 	public float minimumY = -60F;
 	public float maximumY = 60F;
 
-	float rotationY = 0F;
+	public float rotationY = 0.0F;
 
 	void Update ()
 	{
@@ -43,12 +46,37 @@ public class MouseLook : MonoBehaviour {
 		}
 		else if (axes == RotationAxes.MouseX)
 		{
-			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+            float rotationX = Input.GetAxis("Mouse X") * sensitivityX;
+			
+            if(Input.GetKey(KeyCode.LeftArrow))
+            {
+                rotationX -= sensitivityXKey;
+            }
+
+            if(Input.GetKey(KeyCode.RightArrow))
+            {
+                rotationX += sensitivityXKey;
+            }
+
+            transform.Rotate(0, rotationX, 0);
 		}
 		else
 		{
 			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                rotationY += sensitivityYKey;
+            }
+
+            if(Input.GetKey(KeyCode.DownArrow))
+            {
+                rotationY -= sensitivityYKey;
+            }
+
 			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+
+
 			
 			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
 		}
