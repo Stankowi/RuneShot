42a1545e903fec8363a64f5ed09cd88e9cb35c1a
unknown <altond@SEA-9901687759.ant.amazon.com>
2014-06-03 18:36:11 -0700

    testing purposes: spawn a new vehicle every two minutes

---
+++ Assets/Game/Scripts/Vehicles/VehicleSpawnPoint.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var vehiclePrefab : GameObject;
+var timer : float = 0.0;
+
+function Update() {
+	if (Network.isServer) {
+		this.timer += Time.deltaTime;
+		if (this.timer > 5.0) {
+			Debug.LogError("spawning rover");
+			this.timer = -115.0;
+			NetworkUtil.Instantiate(this.vehiclePrefab, Vector3.zero, Quaternion.identity, 0);
+		}
+	}
+}
